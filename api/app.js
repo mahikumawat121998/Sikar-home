cb8e411182b5f21ee7b75782b1b2ec524224bb58
MAHESH KUMAWAT <mahikumawat121998@gmail.com>
2025-04-23 17:32:44 +0530

    Hosting Sikar home website

--- api/app.js
+++ api/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import session from "express-session";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
@@ -11,8 +10,6 @@ import messageRoute from "./routes/message.route.js";
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
-
-
 app.use(cors({ origin: ["http://localhost:5173","http://54.252.163.13:5173"], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
