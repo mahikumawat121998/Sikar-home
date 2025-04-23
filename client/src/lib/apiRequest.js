bbfcf292ae507bf51257ff3e0843533dd7f6e380
MAHESH KUMAWAT <mahikumawat121998@gmail.com>
2025-04-23 17:24:28 +0530

    Hosting Sikar home website

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  // baseURL: "http://localhost:8800/api",
+  baseURL: "http://54.252.163.13/:8800/api",
   withCredentials: true,
 });
 
